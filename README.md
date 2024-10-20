a6fe2b21975accea8d98116a0deef549b37e2c6c
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-10-20 14:24:33 +0800

    Init repo

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # IE4727-Project
+
+## Getting Started
+
+Install required npm modules
+
+```
+npm install
+```
+
+Run the following command to watch for changes in your files:
+
+```
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+```
+
+Start using Tailwind in your HTML
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "'Apple Color Emoji'",
+          "'Segoe UI Emoji'",
+          "'Segoe UI Symbol'",
+          "'Noto Color Emoji'",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
