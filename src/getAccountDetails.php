8ec1eeac62da069ab4b7230b105cc6648a114cdf
Suu-ly <142672914+Suu-ly@users.noreply.github.com>
2024-11-05 13:50:09 +0800

    Populate order history

--- src/getAccountDetails.php
+++ src/getAccountDetails.php
@@ -6,6 +6,7 @@ require 'db.php';
 
 $details = isset($_GET['details']);
 $user_id = $_SESSION['user_id'];
+header('Content-Type: application/json');
 
 if ($details) {
   $stmt = $conn->prepare("SELECT * FROM users WHERE user_id = ?");
@@ -14,6 +15,21 @@ if ($details) {
   $result = $stmt->get_result();
   $result = $result->fetch_assoc();
 
-  header('Content-Type: application/json');
+  echo json_encode($result);
+} else {
+  $stmt = $conn->prepare("SELECT orders.order_id as id, orders.created_at as date, order_total as total, GROUP_CONCAT(image_url SEPARATOR '|') as image_urls
+                          FROM orders 
+                          INNER JOIN order_items ON order_items.order_id = orders.order_id 
+                          INNER JOIN sizes ON order_items.size_id = sizes.size_id 
+                          INNER JOIN products ON products.product_id = sizes.product_id 
+                          INNER JOIN (SELECT product_id, image_url from images GROUP BY product_id) as i ON i.product_id = products.product_id 
+                          WHERE orders.user_id = ?
+                          GROUP BY orders.order_id
+                          ORDER BY orders.created_at DESC;");
+  $stmt->bind_param("i", $user_id);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $result = $result->fetch_all(MYSQLI_ASSOC);
+
   echo json_encode($result);
 }
